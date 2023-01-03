90b6806fc3f53c99bd5d6de00e53fb522633dbee
jueon <wndjs803@naver.com>
2023-01-03 15:09:01 +0900

    Create number_cardgame_ans1.py

--- Greedy/number_cardgame.py
+++ Greedy/number_cardgame.py
@@ -1,10 +1,14 @@
+#n, m 입력받기
 n, m = map(int, input().split())
+# 입력받은 값을 저장할 list 선언
 matrix = []
 for i in range(n):
     matrix.append(list(map(int, input().split())))
-
+#최소값을 저장할 list 선언
 min_num = []
 for j in matrix:
+    # 각 행마다 가장 작은 수를 append
     min_num.append(min(j)) #min(list) - list 중에 최소값 반환
-    
+
+# list 중 가장 큰 값을 출력    
 print(max(min_num))
