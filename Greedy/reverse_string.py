366b7cf69ecaea0566eaac25fbf690b325b4db16
jueon <wndjs803@naver.com>
2023-01-04 21:35:43 +0900

    Update reverse_string.py

--- Greedy/reverse_string.py
+++ Greedy/reverse_string.py
@@ -1,22 +1,23 @@
 s = input()
-num1 = s.count("0")
-num2 = s.count("1")
-# s에 0과 1중 어느것이 많은가 체크
-if num1 > num2: # 0이 많으면 target = 1
-    target = "1"
-elif num1 == 0 or num2 == 0: # 둘 중하나라도 0개면 target은 없다.
-    target = ""
+num0 = 0 # 1 -> 0 카운트
+num1 = 0 # 0 -> 1 카운트
+
+# pre는 처음 숫자로 지정 후 카운트 증가
+pre = s[0]
+if pre == "0":
+    num0 += 1
 else:
-    target ="0"
+    num1 += 1
 
-count = 0
-pre=s[0]
-
+# 문자열 순회    
 for i in s:
-    if target =="": # target이 없으므로 반복문 탈출
-        break
-    if (pre != i) and (i==target): # 문자가 1->0 또는 0->1로 바뀌었을 때 그 문자가 target과 같다면 교체 횟수 증가
-        count += 1
+    # 다른 숫자로 바뀌는 걸 카운트
+    if(pre != i):
+        if i=="0":
+            num0 += 1
+        else:
+            num1 += 1
     pre = i
-    
-print(count)
+
+# 둘 중 적게 카운트 된 것을 출력
+print(min(num0, num1))
