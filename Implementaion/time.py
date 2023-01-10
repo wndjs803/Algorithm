4b7956fd50ecca379bd49ecea342973a0836e455
jueon <wndjs803@naver.com>
2023-01-10 11:58:48 +0900

    Update time.py

--- Implementaion/time.py
+++ Implementaion/time.py
@@ -5,6 +5,7 @@ time_s = 0
 time_m = 0
 time_h = 0
 count = 0
+
 for i in range(time):
     time_s += 1
     if "3" in str(time_s) or "3" in str(time_m) or "3" in str(time_h):
@@ -15,6 +16,5 @@ for i in range(time):
     if(time_m == 60):
         time_h +=1
         time_m = 0
-    
-print(time_h, time_m, time_s)
+
 print(count)
