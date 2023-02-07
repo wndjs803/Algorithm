e428dd909cb3cea1caf124600eedd6e0bce0c286
jueon <wndjs803@naver.com>
2023-02-07 18:13:37 +0900

    Create n.py

---
+++ 백트래킹/n.py
@@ -0,0 +1,21 @@
+# n=3
+# m=2
+
+n, m = map(int, input().split())
+
+answer = []
+
+def dfs():
+    if (len(answer) == m):
+        for i in answer:
+            print(i, end=" ")
+        print()
+        return
+    
+    for i in range(1, n+1):
+        if i not in answer:
+            answer.append(i)
+            dfs()
+            answer.pop()
+        
+dfs()
