ef6e5813c3cd7dd46264ae42a7949e67a4f9f1e1
Jueon <91904079+wndjs803@users.noreply.github.com>
2024-03-06 16:25:12 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,22 +1,15 @@
-import math
-
 def solution(progresses, speeds):
     answer = []
-    days = []
-    for i in range(len(speeds)):
-        left_prog = 100.0 - progresses[i]
-        day = math.ceil(left_prog/speeds[i])
-        days.append(day)
-
-    max_day = days[0]
-    count = 1
+    index = 0
     
-    for i in range(1, len(days)):
-        if max_day < days[i]:
-            max_day = days[i]
+    while index != len(progresses):
+        count = 0
+        progresses = [a + b for a, b in zip(progresses, speeds)]
+        for progresse in progresses[index:]:
+            if progresse >= 100:
+                count += 1
+                index += 1
+            else: break
+        if count != 0:
             answer.append(count)
-            count = 1
-        else: count += 1
-    
-    answer.append(count)
     return answer
