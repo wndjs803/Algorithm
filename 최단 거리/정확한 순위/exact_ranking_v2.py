fea2102105b5205de662ee6cab18be1d3c4fb1a1
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-06-27 10:50:46 +0900

    Create exact_ranking_v2.py

---
+++ 최단 거리/정확한 순위/exact_ranking_v2.py
@@ -0,0 +1,30 @@
+n, m = map(int, input().split())
+
+INF = int(1e9)
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+for i in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b])
+
+result = 0
+# 각 학생을 번호에 따라 한 명씩 확인하여 도달 가능한지 체크
+for i in range(1, n + 1):
+    count = 0
+    for j in range(1, n + 1):
+        if graph[i][j] != INF or graph[j][i] != INF:
+            count += 1
+    if count == n:
+        result += 1
+print(result)
