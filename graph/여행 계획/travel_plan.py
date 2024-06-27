a215a04205e242c12ad075d8fa4b437c84cdbb7f
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2024-06-27 17:27:28 +0900

    Create travel_plan.py

---
+++ graph/여행 계획/travel_plan.py
@@ -0,0 +1,44 @@
+# 이것이 코딩테스다 with Python p.393
+
+n, m = map(int, input().split())
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+path = list(map(int, input().split()))
+
+parent = [0] * (n + 1)
+for i in range(1, n + 1):
+    parent[i] = i
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 1:
+            if find_parent(parent, i + 1) != find_parent(parent, j + 1):
+                union_parent(parent, i + 1, j + 1)
+
+path = list(set(path))
+check = True
+for i in range(1, len(path)):
+    if parent[path[i]] != parent[path[0]]:
+        check = False
+        break
+
+if check:
+    print("YES")
+else: print("NO")
