97c7e7a2eb34143e9b567710989d4782241dd7ef
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2025-10-16 14:54:05 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        if (ineq.equals("<")) {
+            if (eq.equals("=")) {
+                if (n <= m) return 1;
+            }
+            if (n < m) return 1;
+        }
+        else {
+            if (eq.equals("=")) {
+                if (n >= m) return 1;
+            }
+            if (n > m) return 1;
+        }
+        return 0;
+    }
+}
