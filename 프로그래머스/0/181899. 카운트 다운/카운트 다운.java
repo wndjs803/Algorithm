29a28567af8fc4efda9f3b529f64445cba74f83c
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2026-02-19 19:50:50 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int length = start_num - end_num + 1;
+        int[] answer = new int[length];
+        
+        for(int i = 0; i < length; i++) {
+            answer[i] = start_num;
+            start_num--;
+        }
+        
+        return answer;
+    }
+}
