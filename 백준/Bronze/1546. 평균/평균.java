dd102975fce640155d9cd236f75edcc3aa4187dd
wndjs803 <91904079+wndjs803@users.noreply.github.com>
2026-02-27 18:08:55 +0900

    [Bronze I] Title: 평균, Time: 200 ms, Memory: 18704 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -4,23 +4,19 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        float[] scores = new float[size];
-        float max = -1;
+        int[] scores = new int[size];
+        int max = -1;
+        int sum = 0;
 
         for (int i = 0; i < size; i++) {
-            scores[i] = sc.nextFloat();
+            scores[i] = sc.nextInt();
+            sum += scores[i];
 
             if (max <= scores[i]) {
                 max = scores[i];
             }
         }
 
-        float sum = 0;
-        for (float score : scores) {
-            sum += (score / max) * 100;
-        }
-
-        System.out.println(sum / size);
-
+        System.out.println(sum * 100.0 / max / size);
     }
 }
